60cca8d860f7bdd52ecb6c75d234c5bfad164994
nancy4358 <as30528305@gmail.com>
2025-06-24 00:56:52 +0800

    調整banner

--- frontend/src/components/BannerSlider.jsx
+++ frontend/src/components/BannerSlider.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { supabase } from "@/api/supabase";
+import supabase from '@/api/supabase';
 import { useLocation } from 'react-router-dom';
 import './BannerSlider.css'; 
 
